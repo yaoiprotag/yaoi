22b9ff503faee27fc44f2205ac8dd085fa64b827
nate <131909495+n-jramirez@users.noreply.github.com>
2023-06-12 12:49:52 -0700

    Update

--- g/cookieclicker/game/main.js
+++ g/cookieclicker/game/main.js
@@ -1927,9 +1927,9 @@ Game.Launch=function()
 	{
 		l('offGameMessage').innerHTML=
 		'<div class="title">Oops. Wrong address!</div>'+
-		'<div>It looks like you\'re accessing Cookie Clicker from another URL than the official one.<br>'+
-		'You can <a href="//orteil.dashnet.org/cookieclicker/" target="_blank">play Cookie Clicker over here</a>!<br>'+
-		'<small>(If for any reason, you are unable to access the game on the official URL, we are currently working on a second domain.)</small></div>';
+		'<div>It looks like you\'re accessing Cookie Clicker from another URL<br>'+
+		'You can <a href="/g/cookieclicker/game/" target="_blank">play Cookie Clicker over here</a>!<br>'+
+		'<small>Thanks for using nate-games!</small></div>';
 	}
 	Game.timedout=false;
 	Game.Timeout=function()
